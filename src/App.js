0bd5513ce6991e8a0e41949ec24c01c1b675beba
Mikael B <borges.mikael@gmail.com>
2022-01-12 15:57:58 +0100

    cleaning pour commencer react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Todo from "./components/Todo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>todos</h1>
-      <Todo title="a" />
-      <Todo title="b" />
-      <Todo title="c" />
+      hello
     </div>
   );
 }
