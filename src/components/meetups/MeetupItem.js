c11d9a282c3e3fea5d4198760c3ff0d0b299256a
MikaelBorges <borges.mikael@gmail.com>
2022-01-25 20:47:23 +0100

    composants wrapped

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,27 @@
-import styles from './MeetupItem.module.css';
+import styles from "./MeetupItem.module.css";
+import Card from "../ui/Card";
 
 function MeetupItem(props) {
+
+  const addToFavorites = () => {
+    console.log("add to favorites");
+  }
+
   return (
     <li className={styles.meetup}>
-      <div>
-        <img src={props.image} />
-      </div>
-      <div>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div>
+          <img src={props.image} />
+        </div>
+        <div>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div>
+          <button onClick={addToFavorites}>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
