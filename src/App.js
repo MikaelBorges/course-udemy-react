f53d952c3b4be0bcfd0e6b542ff73b906c3a8e69
MikaelBorges <borges.mikael@gmail.com>
2022-01-31 22:58:36 +0100

    nombre total de meetups en notification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import { Route, Switch } from 'react-router-dom';
-
+import { useState } from 'react';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 import Layout from './components/layout/Layout';
 
 function App() {
+
+  const [totalMeetups, setTotalMeetups] = useState(0);
+
+  function changeStateMeetups(meetups) {
+    setTotalMeetups(meetups);
+  }
+
   return (
-    <Layout>
+    <Layout meetupsLength={totalMeetups}>
       <Switch>
         <Route path='/' exact>
-          <AllMeetupsPage />
+          <AllMeetupsPage changeMeetups={changeStateMeetups} />
         </Route>
         <Route path='/new-meetup'>
           <NewMeetupPage />
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,7 +4,7 @@ import MainNavigation from './MainNavigation';
 function Layout(props) {
   return (
     <div className={styles.layout}>
-      <MainNavigation />
+      <MainNavigation allMeetups={props.meetupsLength} />
       <main>
         {props.children}
       </main>
