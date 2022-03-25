e73106938b3b8a8aff996920e432aae8e8e161ab
MikaelBorges <borges.mikael@gmail.com>
2022-03-26 00:40:30 +0100

    ajout d'emojis

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,7 +4,7 @@ import MainNavigation from './MainNavigation';
 function Layout(props) {
   return (
     <div className={styles.layout}>
-      <button className={styles.themeButton}>theme</button>
+      <button className={styles.themeButton}>theme 👷‍♂️</button>
       <MainNavigation allMeetups={props.meetupsLength} />
       <main>
         {props.children}
--- src/components/meetups/FilterCards.js
+++ src/components/meetups/FilterCards.js
@@ -1,7 +1,7 @@
 import styles from './FilterCards.module.css';
 
 function FilterCards() {
-  return <p className={styles.filterZone}>filter zone</p>;
+  return <p className={styles.filterZone}>filter zone 👷‍♂️</p>;
 }
 
 export default FilterCards;
